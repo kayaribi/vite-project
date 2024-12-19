b5e7ffeaeeda0cd8e0a99e61a2c91dded8da8b17
Kaya <kaya_chen@outlook.com>
2024-12-19 22:53:39 +0800

    新增

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   //開發中、產品路徑
-  base: process.env.MODE_ENV === 'production' ? '' : ''
+  base: process.env.NODE_ENV === 'production' ? '/vite-project-sample/' : '',
   plugins: [react()],
 })
